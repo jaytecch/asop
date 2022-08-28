7aae54e010e68fea78b388a8eb014b8b855dc5ae
Berkay ÇIRAK <berkaycirak_11@hotmail.com>
2022-08-28 21:54:27 +0300

    Its done

--- src/pages/SellNFT.jsx
+++ src/pages/SellNFT.jsx
@@ -2,56 +2,63 @@ import CoinImg from '../assets/10A_Front.png';
 import ProcessInput from '../components/ProcessInput';
 import PriceInput from '../components/PriceInput';
 import { useState } from 'react';
+import qrCode from '../assets/qr_code.jpeg';
 
 function SellNFT() {
-  const [selected, setSelected] = useState(null);
+	const [selected, setSelected] = useState(null);
 
-  const handleClick = (tier) => {
-    if (selected === tier) {
-      return setSelected(null);
-    }
-    setSelected(tier);
-  };
-  return (
-    <div className='flex flex-col justify-center md:flex-row'>
-      <div className='flex flex-col justify-center items-center cursor-pointer '>
-        <img src={CoinImg} alt='' className='h-80' />
-        <div className=''>
-          <ProcessInput
-            tier={1}
-            process={'sell'}
-            onClick={() => handleClick(1)}
-            selected={selected}
-          />
-          <PriceInput price={85} unit={'$'} />
-        </div>
-      </div>
-      <div className='flex flex-col justify-center items-center cursor-pointer '>
-        <img src={CoinImg} alt='' className='h-80' />
-        <div>
-          <ProcessInput
-            tier={5}
-            process={'sell'}
-            onClick={() => handleClick(5)}
-            selected={selected}
-          />
-          <PriceInput price={425} unit={'$'} />
-        </div>
-      </div>
-      <div className='flex flex-col justify-center items-center cursor-pointer '>
-        <img src={CoinImg} alt='' className='h-80' />
-        <div className=''>
-          <ProcessInput
-            tier={10}
-            process={'sell'}
-            onClick={() => handleClick(10)}
-            selected={selected}
-          />
-          <PriceInput price={850} unit={'$'} />
-        </div>
-      </div>
-    </div>
-  );
+	const handleClick = (tier) => {
+		if (selected === tier) {
+			return setSelected(null);
+		}
+		setSelected(tier);
+	};
+	return (
+		<div className='flex flex-col justify-center md:flex-row'>
+			<div className='flex flex-col justify-center items-center cursor-pointer '>
+				<img src={CoinImg} alt='' className='h-80' />
+				<div className=''>
+					<ProcessInput
+						tier={1}
+						process={'sell'}
+						onClick={() => handleClick(1)}
+						selected={selected}
+					/>
+					<img src={qrCode} alt='' className='h-20 mx-auto mb-4' />
+
+					<PriceInput price={85} unit={'$'} />
+				</div>
+			</div>
+			<div className='flex flex-col justify-center items-center cursor-pointer '>
+				<img src={CoinImg} alt='' className='h-80' />
+				<div>
+					<ProcessInput
+						tier={5}
+						process={'sell'}
+						onClick={() => handleClick(5)}
+						selected={selected}
+					/>
+					<img src={qrCode} alt='' className='h-20 mx-auto mb-4' />
+
+					<PriceInput price={425} unit={'$'} />
+				</div>
+			</div>
+			<div className='flex flex-col justify-center items-center cursor-pointer '>
+				<img src={CoinImg} alt='' className='h-80' />
+				<div className=''>
+					<ProcessInput
+						tier={10}
+						process={'sell'}
+						onClick={() => handleClick(10)}
+						selected={selected}
+					/>
+					<img src={qrCode} alt='' className='h-20 mx-auto mb-4' />
+
+					<PriceInput price={850} unit={'$'} />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default SellNFT;
