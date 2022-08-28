8819ba4420491c697ae444068bfa81b67b74b5e0
Berkay <berkaycirak_11@hotmail.com>
2022-08-28 14:14:34 +0300

    Hamburger is done

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,26 +1,40 @@
-import Logo from '../../assets/Logo.svg';
 import { Link, useLocation } from 'react-router-dom';
-import { useState } from 'react';
-import Hamburger from '../Hamburger';
 
-function Header() {
+function Hamburger({ isClicked, handleClick }) {
   const location = useLocation();
-  const [isClicked, setIsClicked] = useState(false);
-
-  const handleClick = () => {
-    setIsClicked(!isClicked);
-  };
   return (
-    <div className='bg-orange-300 '>
-      <div className=' container mx-auto flex items-center gap-x-[12rem] relative   '>
-        <Link to='/'>
-          <img src={Logo} alt='logo' className='h-56' />
-        </Link>
-
-        <nav className='hidden md:flex text-lg uppercase font-semibold absolute bottom-0 right-[10%]  '>
+    <div className='md:hidden mr-10 '>
+      <div
+        className=' cursor-pointer border-1 border-white relative '
+        onClick={handleClick}
+      >
+        <div
+          className={`relative z-20 w-10 h-[33px] transform rotate-0 transition duration-50 ease-in-out cursor-pointer  `}
+        >
+          <span
+            className={`bg-white block absolute w-full h-[5px] rounded-[7px] opacity-100 left-0 transform rotate-0 transition duration-250 ease-in-out origin-[3px_1px] top-0 ${
+              isClicked && '-top-0  rotate-45'
+            }`}
+          ></span>
+          <span
+            className={`bg-white block absolute w-full h-[5px] rounded-[7px] opacity-100 left-0 transform rotate-0 transition duration-250 ease-in-out origin-left-center top-[14px] ${
+              isClicked && 'w-0 opacity-0'
+            }`}
+          ></span>
+          <span
+            className={`bg-white block absolute w-full h-[5px] rounded-[7px] opacity-100 left-0 transform rotate-0 transition duration-250 ease-in-out origin-[-3px_1px] top-7 ${
+              isClicked && '-rotate-45'
+            }`}
+          ></span>
+        </div>
+        <div
+          className={`h-[300px] w-[280px] bg-white absolute right-10 z-10 transition-all duration-500 rounded-xl flex flex-col font-bold justify-center text-center  ${
+            isClicked ? 'opacity-100' : 'opacity-0'
+          } `}
+        >
           <Link
             to='/mint'
-            className={`transition-all hover:bg-gray-200 hover:opacity-100 border-2 rounded-lg p-4 ${
+            className={`transition-all hover:bg-gray-200 hover:opacity-100  p-4 ${
               location.pathname === '/mint'
                 ? ' bg-gray-200 opacity-100'
                 : 'opacity-80 '
@@ -68,11 +82,10 @@ function Header() {
           >
             FAQ
           </Link>
-        </nav>
-        <Hamburger isClicked={isClicked} handleClick={handleClick} />
+        </div>
       </div>
     </div>
   );
 }
 
-export default Header;
+export default Hamburger;
